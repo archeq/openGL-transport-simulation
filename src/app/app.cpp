54d45aad94e5dc9c2e99d6fdbfde1ef308f688aa
KUCIA <noiszewczyk@gmail.com>
2025-06-20 04:45:10 +0200

    init: app sceleton

---
+++ src/app/app.cpp
@@ -0,0 +1,81 @@
+//
+// Created by KUCIA on 20.06.2025.
+//
+
+#include "app.h"
+
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#define WINDOW_WIDTH 1600
+#define WINDOW_HEIGHT 900
+#define WINDOW_TITLE "Simulation"
+
+
+void App::run() {
+
+    // initialization
+    glfw_init();
+    window = window_init(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glad_init();
+
+    // running the loop
+    loop();
+
+}
+
+void App::terminate() {
+
+    glfwTerminate();
+    std::cout << "App was terminated" << std::endl;
+
+}
+
+void App::glfw_init() {
+
+    if (is_glfw_initialized) return;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    is_glfw_initialized = true;
+
+}
+
+void App::glad_init() {
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        terminate();
+        return;
+    }
+
+    is_glad_initialized = true;
+
+}
+
+GLFWwindow* App::window_init(int width, int height) {
+
+    GLFWwindow* window = glfwCreateWindow(width, height, WINDOW_TITLE, nullptr, nullptr);
+    if (!window) {
+
+        std::cout << "Failed to create GLFW window" << std::endl;
+        terminate();
+
+    }
+    glfwMakeContextCurrent(window);
+    return window;
+
+}
+
+void App::loop() {
+
+    while (!glfwWindowShouldClose(window)) {
+
+    }
+
+}
---
+++ src/app/app.h
@@ -0,0 +1,31 @@
+//
+// Created by KUCIA on 20.06.2025.
+//
+
+#ifndef APP_H
+#define APP_H
+#include "GLFW/glfw3.h"
+
+
+class App {
+
+public:
+
+    GLFWwindow* window = nullptr;
+
+    void run();  // initializes the app and starts its main loop
+    static void terminate();  // kills the app
+    void glfw_init();  // pretty much self-explanatory
+    void glad_init();
+
+private:
+
+    bool is_glfw_initialized = false;
+    bool is_glad_initialized = false;
+
+    GLFWwindow* window_init(int width, int height);
+    void loop();
+
+};
+
+#endif //APP_H
