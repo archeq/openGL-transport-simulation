aba496551056d11c5e077eb46fc92622628795cf
KUCIA <noiszewczyk@gmail.com>
2025-06-27 07:49:13 +0200

    add: Shader header

---
+++ src/view/Shader.h
@@ -0,0 +1,26 @@
+//
+// Created by KUCIA on 22.06.2025.
+//
+
+#ifndef SHADER_H
+#define SHADER_H
+#include <stddef.h>
+
+
+class Shader {
+public:
+
+    unsigned int id = NULL;
+    Shader(const char *vertexPath, const char *fragmentPath);
+    void use();
+
+    // uniform management
+    void setBool(const char *name, bool value);
+    void setInt(const char *name, int value);
+    void setFloat(const char *name, float value);
+
+};
+
+
+
+#endif //SHADER_H
