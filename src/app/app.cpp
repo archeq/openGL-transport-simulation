c7b03f9a35fe65604a41d3cfa6503aa2337b26b1
KUCIA <noiszewczyk@gmail.com>
2025-07-01 17:54:12 +0200

    drawing the model

--- src/app/app.cpp
+++ src/app/app.cpp
@@ -18,6 +18,7 @@
 #include "glm/glm.hpp"
 #include "glm/ext/matrix_clip_space.hpp"
 #include "glm/gtx/transform.hpp"
+#include "stb/stb_image.h"
 
 #define WINDOW_WIDTH 1600
 #define WINDOW_HEIGHT 900
@@ -82,6 +83,8 @@ void App::run() {
     // glad (openGL) initialization
     glad_init();
 
+    stbi_set_flip_vertically_on_load(true);
+
     // depth
     glEnable(GL_DEPTH_TEST);
 
@@ -132,20 +135,21 @@ GLFWwindow *App::window_init(int width, int height) {
     return window;
 }
 
-Shader shader, lightSourceShader;
+Shader simpleShader, lightSourceShader, modelShader;
 unsigned int VBO, cubeVAO, lightCubeVAO;
 Texture boxTexture, boxSpecularMap;
 Camera camera;
-Model backpack;
+Model backpackModel;
 
 void test_setup() {
     // Temporal function for TESTING only
-    shader = Shader("../shaders/specular_map.vert", "../shaders/specular_map.frag");
+    simpleShader = Shader("../shaders/specular_map.vert", "../shaders/specular_map.frag");
     lightSourceShader = Shader("../shaders/light_source.vert", "../shaders/light_source.frag");
+    modelShader = Shader("../shaders/model_shader.vert", "../shaders/model_shader.frag");
     boxTexture = Texture(std::string("../textures/container.png"));
     boxSpecularMap = Texture(std::string("../textures/container2_specular.png"));
     camera = Camera(glm::vec3(0.0f, 0.0f, 3.0f));
-    backpack = Model("../models/backpack/backpack.obj");
+    backpackModel = Model("../models/backpack/backpack.obj");
 
     // test box
     glGenVertexArrays(1, &cubeVAO);
@@ -221,32 +225,32 @@ void draw_test_box(const glm::vec3 &light_pos, const glm::mat4 &projection, cons
     // ======================================
     //              test box
     // ======================================
-    shader.use();
-    shader.setVec3("light.position", light_pos);
-    shader.setVec3("viewPos", camera.Position);
+    simpleShader.use();
+    simpleShader.setVec3("light.position", light_pos);
+    simpleShader.setVec3("viewPos", camera.Position);
 
     // light properties
-    shader.setVec3("light.ambient", glm::vec3(0.3f, 0.3f, 0.3f));
-    shader.setVec3("light.diffuse", glm::vec3(0.5f, 0.5f, 0.5f));
-    shader.setVec3("light.specular", glm::vec3(1.0f, 1.0f, 1.0f));
+    simpleShader.setVec3("light.ambient", glm::vec3(0.3f, 0.3f, 0.3f));
+    simpleShader.setVec3("light.diffuse", glm::vec3(0.5f, 0.5f, 0.5f));
+    simpleShader.setVec3("light.specular", glm::vec3(1.0f, 1.0f, 1.0f));
 
     // material properties
     // shader.setVec3("material.specular", glm::vec3(0.5f, 0.5f, 0.5f));  // for shader without specular mapping
-    shader.setFloat("material.shininess", 80.0f);
+    simpleShader.setFloat("material.shininess", 80.0f);
 
-    shader.setMat4("projection", projection);
-    shader.setMat4("view", view);
+    simpleShader.setMat4("projection", projection);
+    simpleShader.setMat4("view", view);
 
     auto model = glm::mat4(1.0f);
-    shader.setMat4("model", model);
+    simpleShader.setMat4("model", model);
 
     // bind diffuse map
-    shader.setInt("material.diffuse", 0);
+    simpleShader.setInt("material.diffuse", 0);
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, boxTexture.id);
 
     // bind specular map
-    shader.setInt("material.specular", 1);
+    simpleShader.setInt("material.specular", 1);
     glActiveTexture(GL_TEXTURE1);
     glBindTexture(GL_TEXTURE_2D, boxSpecularMap.id);
 
@@ -283,11 +287,29 @@ void render(GLFWwindow *window) {
     auto view = camera.GetViewMatrix();
 
     // test box
-    draw_test_box(light_pos, projection, view);
+    // draw_test_box(light_pos, projection, view);
+
+    // test model
+    // TODO: я это все скоро красиво организую в class Model
+    auto model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+    model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
+    modelShader.use();
+    modelShader.setMat4("model", model);
+    modelShader.setMat4("view", view);
+    modelShader.setMat4("projection", projection);
+    modelShader.setVec3("light.position", light_pos);
+    modelShader.setVec3("light.ambient", glm::vec3(0.3f, 0.3f, 0.3f));
+    modelShader.setVec3("light.diffuse", glm::vec3(0.5f, 0.5f, 0.5f));
+    modelShader.setVec3("light.specular", glm::vec3(1.0f, 1.0f, 1.0f));
+    modelShader.setVec3("viewPos", camera.Position);
+    modelShader.setFloat("material.shininess", 55.0f);
+
+
+    backpackModel.draw(modelShader);
 
     // light cube
     draw_light_cube(light_pos, projection, view);
-
 
     glUseProgram(0);
 
