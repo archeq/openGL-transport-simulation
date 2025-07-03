4f78e90a6ee893c613f947ef8e3d9543223aaa89
KUCIA <noiszewczyk@gmail.com>
2025-07-03 14:21:47 +0200

    add skybox

---
+++ src/view/Skybox.h
@@ -0,0 +1,29 @@
+//
+// Created by KUCIA on 03.07.2025.
+//
+
+#ifndef SKYBOX_H
+#define SKYBOX_H
+
+#include <string>
+#include <vector>
+
+#include "Shader.h"
+#include "Camera.h"
+
+
+class Skybox {
+public:
+    Skybox() = default;
+    void prepare(const std::vector<std::string> &faces);
+    void draw(const Camera &camera) const;
+
+private:
+    unsigned int texture;
+    Shader shader;
+
+};
+
+
+
+#endif //SKYBOX_H
